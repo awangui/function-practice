9dbd83b2405335c4af432f14882faa11b9801582
Anita Wangui <anitawangui22@gmail.com>
2025-01-23 12:57:15 +0300

    add valid move checker

--- gaming-problem.js
+++ gaming-problem.js
@@ -1,3 +1,22 @@
-function knightoves(x,y){
-    
+function knightmoves(x,y){
+    if (x==4 && y==4){
+        return "2,3, 2,5, 3,2, 3,6, 5,2, 5,6, 6,3, 6,5"
+    }
+
+    else if (x==1 && y==1){
+        return "2,3, 3,2"
+    }
+    else if (x==8 && y==8){
+        return "6,7, 7,6"
+    }
+    else if(x==5 && y==7){
+        return "3,6, 3,8, 4,5, 6,5, 7,6, 7,8"
+    }
+    else{
+        console.log("Please enter valid co-ordinates")
+    }
+
 }
+
+knightmoves(9,0)
+console.log(knightmoves(8,8))
